5cd96c4f2f18aff3b355443ec6fa2cb59830e95b
madmax330 <madmax330@yahoo.com>
2018-01-14 18:12:02 +0000

    added hasbang
    added the shebang and main method for modular exports.

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+#!/usr/bin/env python3
+
 from verification import Verification
 from command import QuitException
 from sqlite3 import Error
 from database import Database
 
-try:
-    db = Database()
-    while True:
-        verify = Verification()
-        var = input("Please enter a command: ")
-        command = verify.verify(var)
-        if command:
-            command.run(db)
-        else:
-            verify.print_errors()
 
-except (QuitException, Error) as e:
-    if isinstance(e, QuitException):
-        print('Thanks for using pypass, until next time!')
-    else:
-        print(str(e))
-        print('Error connecting to the database, please relaunch the application.')
+def main():
+    try:
+        db = Database()
+        while True:
+            verify = Verification()
+            var = input("Please enter a command: ")
+            command = verify.verify(var)
+            if command:
+                command.run(db)
+            else:
+                verify.print_errors()
+
+    except (QuitException, Error) as e:
+        if isinstance(e, QuitException):
+            print('Thanks for using pypass, until next time!')
+        else:
+            print(str(e))
+            print('Error connecting to the database, please relaunch the application.')
+
+
+if __name__ == 'main':
+    main()
 
